9ae740ac7b5435e9e3dade7a4460ce18bc236738
rrvv123 <eorms7391@gmail.com>
2024-04-18 15:01:17 +0900

    고급 위젯 - 아날로그, 디지털, 크로노미터, 타임피커, 데이트피커 등 날짜와 시간과 관련된 예제 - 0418 7교시

---
+++ app/src/main/java/kr/ac/kopo/advancedwidgettest0418/MainActivity.kt
@@ -0,0 +1,16 @@
+package kr.ac.kopo.advancedwidgettest0418
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_date)
+
+    }
+}
